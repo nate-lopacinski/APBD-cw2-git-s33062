814427a36124e6ab9048060cc0111d1a8653e47b
Nate rat Black <nateblack99@host-93-177-89-48.wawtel.pl>
2026-03-21 22:17:21 +0100

    first sketch and work on user started

---
+++ APBD-Cw2-s33062/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Hello, World!");
+
+
+//sprzet
+//laptop projektor ...
+//2+ typy użtkownikow
+//wypozyczenie: moment wypożyczenia, termin zwrotu, faktyczny zwrot oraz wynikające opłaty lub kary.
+
+public abstract class User(String cName,String cSurname)
+{
+    private static int _nextId = 1;
+
+    public int Id { get; set; } = _nextId++;
+    public string Name { get; set; } = cName;
+    public string Surname { get; set; } =  cSurname;
+    
+}
+
+public class Student(String cName, String cSurname) : User(cName, cSurname)
+{
+
+}
